20713648504a72a64de4b13d458b5485c18256fc
MathildeAlice93 <33442642+MathildeAlice93@users.noreply.github.com>
2022-05-18 17:13:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,4 @@
 import pandas as pd
 import streamlit as st
-import plotly.express as pxdf = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges",   
-    "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-    })fig = px.bar(df, x="Fruit", y="Amount", color="City",
-                 barmode="group")st.title('Hello Streamlit')
-st.write('''
-    Streamlit: A web application framework for Python.
-''')
-st.plotly_chart(fig)
+
+print("Hello, World!")
